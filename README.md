5a6056a36d4576cccca390a6df885d21f75f9dd2
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2019-06-04 13:48:10 +0200

    Clean code + Add readme

---
+++ README.md
@@ -0,0 +1,61 @@
+# Leomaradan's Twitter Bot
+## Auto-Tweeting from RSS and auto-retweet
+----
+### Installation
+
+__With git + composer__
+````bash
+git clone https://github.com/Leomaradan/twitter-bot
+cd twitter-bot
+composer install
+````
+
+create the __conf.php__ file, or rename __conf.sample.php__ to __conf.php__
+
+Check the conf.sample.php or schema.json for the availlable option
+
+__Direct download__
+
+go to https://github.com/Leomaradan/twitter-bot/releases and take to latest release.
+
+unzip to file to your server.
+
+create the __conf.php__ file, or rename __conf.sample.php__ to __conf.php__
+
+Check the conf.sample.php or schema.json for the availlable option
+
+### Usage
+You have 3 options to run the bot
+* command line
+* in browser
+* included in another script
+
+#### Command line:
+you can execute the bot with the following line :
+
+````bash
+php daemon.php [--verbose] [--simulation] [--html]
+````
+
+* the _--verbose_ option will display debug messages
+* the _--simulation_ option will do everything except sending the tweet and register the lock-file
+* the _--html_ will add &lt;pre&gt;. It can be usefull if used with verbose
+
+#### Browser
+you can call the script directly in a browser, and pass the options as query string
+````url
+daemon.php?html=true&verbose&true
+````
+
+See the command line section for the availlable options
+
+#### Include / Require
+If you want to include the daemon in another script (for example a web-cron), you can declare a variable $daemonConfig with the needed option.
+
+````php
+    ob_start();
+    $daemonConfig = ['simulation' => false];
+    require('daemon.php');
+    $buffer = ob_get_flush();
+    file_put_contents('twitter-bot.log', $buffer, FILE_APPEND | LOCK_EX);
+````
--- conf.sample.php
+++ conf.sample.php
@@ -1,20 +1,50 @@
 <?php
 
+// This configuration need to be validated with schema.json
 return [
-    'account1' => [
+    [
+        'account' => 'account1',
+        'tokens' => [
+            'api_key' => 'abcd',
+            'api_secret' => 'efgh',
+            'access_token' => 'ijkl',
+            'access_secret' => 'mnop',
+        ],
         'retweet' => [
-            'account2',
-            'account3',
-            'account4'
-        ]
+            [
+                'screen' => 'account_name1',
+                'retweet' => true, // include retweet from account_name1
+                'response' => true, // include response to tweet from account_name1
+            ],
+            'account_name2',
+            [
+                'screen' => 'account_name3',
+                'retweet' => false, // ignore retweet
+                'response' => false, // ignore response
+            ],
+        ],
     ],
-    'account2' => [
+    [
+        'account' => 'account1',
+        'tokens' => [
+            'api_key' => 'dcba',
+            'api_secret' => 'hgfe',
+            'access_token' => 'lkji',
+            'access_secret' => 'ponm',
+        ],
         'retweet' => [
-            'account3',
-            'account4'
+            'account_name4',
         ],
         'rss' => [
-            'url to rss'
-        ]
-    ]
+            [
+                'url' => 'https://leomaradan.com/liens/?do=atom',
+                'include_permalink' => true, // include the permalink
+                'include_hashtags' => true, // include the hashtag
+                'parser' => 'shaarli', // the rss is a "shaarli" https://www.shaarli.fr/
+                'filter_hashtag_input' => false, // don't filter any hashtag | if set, will ignore article with these hashtags
+                'filter_hashtag_output' => ['pro'], // filter hashtag output
+            ] .
+            'https://other.exemple.com/rss', // another rss, in automatic mode
+        ],
+    ],
 ];
